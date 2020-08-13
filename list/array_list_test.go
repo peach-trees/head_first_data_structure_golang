b039de6de1da75bfdfc15f34b1c632ce2d4f3c79
xushouming <xushouming@bytedance.com>
2020-08-13 19:59:11 +0800

    array list

--- list/array_list_test.go
+++ list/array_list_test.go
@@ -6,11 +6,11 @@ func TestArrayList_Append(t *testing.T) {
 	tl := NewArrayList(1, 2, 3)
 	t.Log(tl.String())
 
-	//tl.Append('a', 'b', 'c')
-	//t.Log(tl.String())
-	//
-	//tl.Append('x', 'y', 'z')
-	//t.Log(tl.String())
+	tl.Append('a', 'b', 'c')
+	t.Log(tl.String())
+
+	tl.Append('x', 'y', 'z')
+	t.Log(tl.String())
 }
 
 func TestArrayList_Prepend(t *testing.T) {
